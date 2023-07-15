39fd95ce8663c739beca84ad20b9de48ca4e009f
Christian <sefluntok@gmail.com>
2023-07-15 16:26:17 +1000

    Removed: Utility functions for posts and OgImage.

--- src/utils/getPostsbyTag.ts
+++
@@ -1,6 +0,0 @@
-import type { CollectionEntry } from "astro:content";
-
-const getPostsByTag = (posts: CollectionEntry<"blog">[], tag: string) =>
-    posts.filter((post) => post.data.tags.includes(tag));
-
-export default getPostsByTag;
